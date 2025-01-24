fb262b026d0f81ea679c11a991f1ae85b85cdd56
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-24 21:25:11 +0200

    Implement basic server and concet with mongo db

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const ConnectDB = require("./db/db");
+const app = express();
+
+const errorHandlerMiddleware = require("../StoreAPI/middleware/error-handler");
+const notFound = require("./middleware/not-found");
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res)=>{
+    res.send('<h1>STore API</h1>');
+});
+
+//Product routes
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3500;
+
+const start = async () => {
+    try {
+        //Contect to DB
+        await ConnectDB(process.env.MONGO_URL );
+        app.listen(port, console.log(`- Local: http://localhost:${port}/`));
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+start();
