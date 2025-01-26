6e174b3b19322322de342b0707c36d6ec0dd44ec
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-26 17:47:10 +0200

    implementing pagination functionality using page and limit parametars in query

--- controllers/products.js
+++ controllers/products.js
@@ -45,6 +45,13 @@ const getAllProductsUsingQuery = async (req, res) => {
         result = result.select(fieldList);
     }
 
+    // Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
+
     const porducts = await result;
     res.status(200).json({porducts, nbHits:porducts.length});
 };
