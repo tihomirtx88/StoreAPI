b9d60ba367f33a917c9bb65896714a98780b692c
tihomirtx88 <tihomirtx88@gmail.com>
2025-01-26 15:53:22 +0200

    Impleement query functionality to take custom product base on query parametar

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,22 @@
+const Product = require("../models/product");
+
 const getAllProducts = async (req, res) => {
-    res.status(200).json({product: 'Testing product'});
+    const porducts = await Product.find({featured: true});
+    res.status(200).json({porducts, nbHits:porducts.length});
+};
+
+const getAllProductsUsingQuery = async (req, res) => {
+    const { featured } = req.query;
+    const queryObject = {};
+    
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    const porducts = await Product.find(queryObject);
+    res.status(200).json({porducts, nbHits:porducts.length});
 };
 
 module.exports = {
-    getAllProducts
+    getAllProducts,
+    getAllProductsUsingQuery
 };
